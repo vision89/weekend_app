efb3618c2c10e1dd12c02d4391c6e89f2871a87b
dlgulley411@gmail.com <dlgulley411@gmail.com>
2015-04-07 22:12:41 -0400

    Action string is working

--- www/scenes/town/controllers/shrine.controller.js
+++ www/scenes/town/controllers/shrine.controller.js
@@ -5,13 +5,30 @@
  * Town controller
  */
 angular.module( 'sceneMod' ).controller( 'shrineController', ['$scope', 'playerModel', 'eventConstants', 'shrineModel',
-    function ( $scope, playerModel, eventConstants, shrineModel ) {
+    'nounConstants', 'parse',
+    function ( $scope, playerModel, eventConstants, shrineModel, nounConstants, parse ) {
 
         //Service bindings
         $scope.shrineModel = shrineModel;
         $scope.playerModel = playerModel;
         $scope.eventConstants = eventConstants;
         $scope.platform = ionic.Platform.platform().toLowerCase();    //Get the device platform
+        $scope.nounConstants = nounConstants;
+
+        //A card was clicked, we can let parse handle the event if it's ready
+        $scope.cardClicked = function ( noun ) {
+
+            if ( parse.pair.noun === noun ) {
+
+                parse.pair.noun = 0;
+
+            } else {
+
+                parse.pair.noun = noun;
+
+            }
+
+        }
 
     }
 
